9c32f25bccdb34cd718129f2eaacdeb5dfb30587
Johannes Wagener <johannes@wagener.cc>
2011-11-03 16:52:23 -0700

    use canvas waveform WIP

--- javascripts/application.coffee
+++ javascripts/application.coffee
@@ -1,9 +1,38 @@
-CLIENT_ID = "1ba7ea8a06c6bb7454a52fb018449792"
+CLIENT_ID = "7b3dc769ad5c179d5280de288dba52a9"
 
 GR =
   groupId: null
   groupUrl: null
   
+  drawWaveform: (canvas, waveformUrl) -> 
+    color = [255, 0, 0]
+    waveImg = new Image()
+    waveImg.src = "canvas/wave.png"#waveformUrl
+    waveImg.onload = () ->
+      GR.drawOverlay(this, canvas, color)
+    
+  drawOverlay: (imageObj, canvas, color) ->
+    parent = canvas.parentNode;
+    canvas.width = parent.offsetWidth;
+    canvas.height = parent.offsetHeight;
+  
+    context = canvas.getContext("2d");
+  
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    data = imageData.data;
+
+    i =0 
+    while i < data.length #for (i = 0; i < data.length; i += 4)
+      if (data[i + 3]) # If solid 
+        data[i]     = color[0]; # red
+        data[i + 1] = color[1]; # green
+        data[i + 2] = color[2]; # blue             
+      i += 4
+
+    # overwrite original image
+    context.putImageData(imageData, 0, 0);
+      
 $ ->
   SC.initialize
     client_id: CLIENT_ID
@@ -14,5 +43,24 @@ $ ->
   SC.get GR.groupUrl, (group) ->
     $(".groupLink").text(group.name).attr("href", group.permalink_url)
 
-  SC.get GR.groupUrl + "/tracks", {limit: 5}, (groups) ->
-    $( "#trackTmpl" ).tmpl(groups).appendTo(".track-list ol");
+  SC.get GR.groupUrl + "/tracks", {limit: 5}, (tracks) ->
+    #$("#trackTmpl").tmpl(tracks).appendTo(".track-list ol")
+    for track in tracks
+      trackLi = $("#trackTmpl").tmpl(track).appendTo(".track-list ol")
+      GR.drawWaveform trackLi.find("canvas")[0], track.waveform_url
+
+
+$(".trackLink").live "click", (e) ->
+  $a = $(this)
+  $li = $a.closest("li")
+
+  # stop any sound
+
+  if $li.hasClass("playing")
+    $li.removeClass("playing")
+  else
+    $li.addClass("playing").siblings().removeClass("playing")
+    SC.stream($a.attr("href"), {auto_play: true})
+
+  e.preventDefault()
+  
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,37 @@
 var CLIENT_ID, GR;
-CLIENT_ID = "1ba7ea8a06c6bb7454a52fb018449792";
+CLIENT_ID = "7b3dc769ad5c179d5280de288dba52a9";
 GR = {
   groupId: null,
-  groupUrl: null
+  groupUrl: null,
+  drawWaveform: function(canvas, waveformUrl) {
+    var color, waveImg;
+    color = [255, 0, 0];
+    waveImg = new Image();
+    waveImg.src = "canvas/wave.png";
+    return waveImg.onload = function() {
+      return GR.drawOverlay(this, canvas, color);
+    };
+  },
+  drawOverlay: function(imageObj, canvas, color) {
+    var context, data, i, imageData, parent;
+    parent = canvas.parentNode;
+    canvas.width = parent.offsetWidth;
+    canvas.height = parent.offsetHeight;
+    context = canvas.getContext("2d");
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    data = imageData.data;
+    i = 0;
+    while (i < data.length) {
+      if (data[i + 3]) {
+        data[i] = color[0];
+        data[i + 1] = color[1];
+        data[i + 2] = color[2];
+      }
+      i += 4;
+    }
+    return context.putImageData(imageData, 0, 0);
+  }
 };
 $(function() {
   var params;
@@ -18,7 +47,28 @@ $(function() {
   });
   return SC.get(GR.groupUrl + "/tracks", {
     limit: 5
-  }, function(groups) {
-    return $("#trackTmpl").tmpl(groups).appendTo(".track-list ol");
+  }, function(tracks) {
+    var track, trackLi, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = tracks.length; _i < _len; _i++) {
+      track = tracks[_i];
+      trackLi = $("#trackTmpl").tmpl(track).appendTo(".track-list ol");
+      _results.push(GR.drawWaveform(trackLi.find("canvas")[0], track.waveform_url));
+    }
+    return _results;
   });
+});
+$(".trackLink").live("click", function(e) {
+  var $a, $li;
+  $a = $(this);
+  $li = $a.closest("li");
+  if ($li.hasClass("playing")) {
+    $li.removeClass("playing");
+  } else {
+    $li.addClass("playing").siblings().removeClass("playing");
+    SC.stream($a.attr("href"), {
+      auto_play: true
+    });
+  }
+  return e.preventDefault();
 });
